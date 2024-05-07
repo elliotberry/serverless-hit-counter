024d94bf3c6d4c46df74ff71148bf05a583a1b1b
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-05-07 15:11:00 -0400

    new progress bits

--- index.js
+++ index.js
@@ -1,54 +1,59 @@
-
 import TextToSVG from './ttsvg3.js';
 
-async function incrementCounter(env) {
-  let currentValue = await env.kv.get('count');
+import { AutoRouter, cors, withParams } from 'itty-router'
+
+// get preflight and corsify pair
+const { preflight, corsify } = cors()
+
+const router = AutoRouter({
+  before: [preflight],  // add preflight upstream
+  finally: [corsify],   // and corsify downstream
+})
+
+async function incrementCounter(env, key) {
+  let currentValue = await env.kv.get(`${key}-count`);
   let newValue = 1;
   if (currentValue) {
     newValue = parseInt(currentValue) + 1;
   }
- 
-  await env.kv.put('count', newValue.toString());
+
+  await env.kv.put(`${key}-count`, newValue.toString());
   return newValue;
 }
 
-async function getCurrentCount(env) {
-  const value = await env.kv.get('count');
-  return value ? parseInt(value) : 0;
+const makeSVG = async (text, env) => {
+  let buffer = await env.ttf.get('test.ttf', {type: 'arrayBuffer'});
+  const textToSVG = TextToSVG.loadSync(buffer);
+
+  const attributes = {fill: 'red', stroke: 'black'};
+  const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
+
+  const svg = textToSVG.getSVG(text, options);
+  return svg;
 }
 
-function renderSVG(count) {
-  return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="50">
-      <text x="10" y="30" font-family="Arial" font-size="16">
-        Hits: ${count}
-      </text>
-    </svg>
-  `;
-}
 
-export default {
-  async fetch(request, env, ctx) {
-  
-     let value = await incrementCounter(env);
+router.get('/', async (req, env, ctx) => {
+  let svg = await makeSVG('Hello, World!', env);
+  return new Response(svg, {
+    headers: {
+      'Content-Type': 'image/svg+xml',
+    },
+  });
+})
 
- 
-  
-    let buffer = await env.ttf.get("test.ttf", { type: "arrayBuffer" });
+router.get('/:key', withParams, async (req, env, ctx) => {
+  let key = req.params.key;
+  let value = await incrementCounter(env, key);
 
+  let svg = await makeSVG(`${value}`, env);
 
-   const textToSVG = TextToSVG.loadSync(buffer);
-   
-    const attributes = {fill: 'red', stroke: 'black'};
-    const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
-     
-    const svg = textToSVG.getSVG(`${value}`, options);
-  
-    // Return SVG response
-    return new Response(svg, {
-      headers: {
-        'Content-Type': 'image/svg+xml',
-      },
-    });
-  },
-};
+  // Return SVG response
+  return new Response(svg, {
+    headers: {
+      'Content-Type': 'image/svg+xml',
+    },
+  });
+});
+
+export default router;
